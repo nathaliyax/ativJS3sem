5c3055ea19d771b9df50f76def6df3b670d8d514
nathaliyax <nathalialimas@hotmail.com>
2026-04-16 13:25:40 -0300

    feat: added lesson 4

---
+++ atv_4/app.js
@@ -0,0 +1,20 @@
+let mes = 4
+let nomeMes = ""
+
+switch (mes) {
+    case 1: nomeMes = "Janeiro"; break
+    case 2: nomeMes = "Fevereiro"; break
+    case 3: nomeMes = "Março"; break
+    case 4: nomeMes = "Abril"; break
+    case 5: nomeMes = "Maio"; break
+    case 6: nomeMes = "Junho"; break
+    case 7: nomeMes = "Julho"; break
+    case 8: nomeMes = "Agosto"; break
+    case 9: nomeMes = "Setembro"; break
+    case 10: nomeMes = "Outubro"; break
+    case 11: nomeMes = "Novembro"; break
+    case 12: nomeMes = "Dezembro"; break
+    default: nomeMes = "Mês invalido"
+}
+
+console.log(`O mês selecionado é: ${nomeMes}`)
